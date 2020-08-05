5a9a768888524e2df54bed4551541f83e63178c1
Stipo Margic <stipo@liberato.io>
2020-08-05 15:08:38 +0200

    Modal and download btn

--- src/Components/ImageModal.jsx
+++ src/Components/ImageModal.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const ImageModal = () => {
-  return <div></div>;
+const ImageModal = ({ clickedImage, setClickedImage }) => {
+  const downloadImage = () => {
+    fetch(`https://cors-anywhere.herokuapp.com/${clickedImage.url}`).then(
+      (response) => {
+        response.blob().then((blob) => {
+          let url = window.URL.createObjectURL(blob);
+          let a = document.createElement('a');
+          a.href = url;
+          a.download = clickedImage.id;
+          a.click();
+        });
+      }
+    );
+  };
+
+  return (
+    <div onClick={() => setClickedImage(null)} className='backdrop'>
+      <img src={clickedImage.url} alt='test' />
+      <div className='download'>
+        <button onClick={downloadImage}>DOWNLOAD</button>
+      </div>
+    </div>
+  );
 };
 
 export default ImageModal;
--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -4,15 +4,13 @@ import { useFirestore } from '../hooks/firebase';
 const Images = ({ setClickedImage }) => {
   const { docs } = useFirestore('images');
 
-  console.log(docs);
-
   return (
     <div className='img-grid'>
       {docs &&
         docs.map((doc) => (
           <div className='img-wrap' key={doc.id}>
             <img
-              onClick={() => setClickedImage(doc.url)}
+              onClick={() => setClickedImage(doc)}
               src={doc.url}
               alt='test'
             />
